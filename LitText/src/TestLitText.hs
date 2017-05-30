47bcc585fbcf6712ec8633902471ecf4cd275fde
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2017-05-30 22:36:48 +0200

    all tests run

--- LitText/src/TestLitText.hs
+++ LitText/src/TestLitText.hs
@@ -18,8 +18,8 @@ import   {-@ HTF_TESTS @-} Lines2para.HandleLayout
 import   {-@ HTF_TESTS @-} Lines2para.Lines2ignore
 import   {-@ HTF_TESTS @-} Lines2para.Lines2para
 import   {-@ HTF_TESTS @-} Parser.ProduceLit
---import {-@ HTF_TESTS @-} Processor.CheckServers
---
+import {-@ HTF_TESTS @-} Processor.CheckServers
+
 import   {-@ HTF_TESTS @-} Parser.ProduceDocCallNLP   -- calls to NLP
 import   {-@ HTF_TESTS @-} Parser.ProduceNLP
 import   {-@ HTF_TESTS @-} Parser.ProduceNLPtriples
