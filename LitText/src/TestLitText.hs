d6b2a95f77aabe66696481a75f0dbf21c588ec46
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2017-05-30 10:47:50 +0200

    run all tests -- ok except 5

--- LitText/src/TestLitText.hs
+++ LitText/src/TestLitText.hs
@@ -12,17 +12,17 @@ module Main     where      -- must have Main (main) or Main where
 
 import Uniform.Strings
 import                   Test.Framework
---import {-@ HTF_TESTS @-} Parser.ReadMarkupAB
---import {-@ HTF_TESTS @-} BuchCode.MarkupText
---import   {-@ HTF_TESTS @-} Lines2para.HandleLayout
---import   {-@ HTF_TESTS @-} Lines2para.Lines2ignore
---import   {-@ HTF_TESTS @-} Lines2para.Lines2para
---import   {-@ HTF_TESTS @-} Parser.ProduceLitt
+import {-@ HTF_TESTS @-} Parser.ReadMarkupAB
+import {-@ HTF_TESTS @-} BuchCode.MarkupText
+import   {-@ HTF_TESTS @-} Lines2para.HandleLayout
+import   {-@ HTF_TESTS @-} Lines2para.Lines2ignore
+import   {-@ HTF_TESTS @-} Lines2para.Lines2para
+import   {-@ HTF_TESTS @-} Parser.ProduceLit
 --import {-@ HTF_TESTS @-} Processor.CheckServers
 --
---import   {-@ HTF_TESTS @-} Parser.ProduceDocCallNLP   -- calls to NLP
+import   {-@ HTF_TESTS @-} Parser.ProduceDocCallNLP   -- calls to NLP
 import   {-@ HTF_TESTS @-} Parser.ProduceNLP
---import   {-@ HTF_TESTS @-} Parser.ProduceNLPtriples
+import   {-@ HTF_TESTS @-} Parser.ProduceNLPtriples
 --import   {-@ HTF_TESTS @-} Parser.ConvertTaggerOutput
 --import   {-@ HTF_TESTS @-} Parser.CompleteSentence
 ------ calls 17701
