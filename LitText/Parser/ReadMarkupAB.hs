5e6e97d9e74432d78d784ade3cb84036f02556b9
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2017-07-26 22:27:41 +0200

    more missing tests

--- LitText/Parser/ReadMarkupAB.hs
+++ LitText/Parser/ReadMarkupAB.hs
@@ -102,14 +102,14 @@ bomWarning v = do  -- not required - parser filter it out
 testDataDir = makeAbsDir  "/home/frank/Workspace8/LitTextWorkGeras/LitTextWork/TestData"
         :: Path Abs Dir
 
---test_0_A_B_textstate_text_1 =   testVar2File result0A "resultB0" textstate2Text
---test_1_A_B_textstate_text_1 =   testVar2File result1A "resultB1" textstate2Text
---test_2_A_B_textstate_text_2 =   testVar2File result2A "resultB2" textstate2Text
---test_3_A_B_textstate_text_3 =   testVar2File result3A "resultB3" textstate2Text
---test_4_A_B_textstate_text_4 =   testVar2File result4A "resultB4" textstate2Text
---test_5_A_B_textstate_text_5 =   testVar2File result5A "resultB5" textstate2Text
---test_6_A_B_textstate_text_6 =   testVar2File result6A "resultB6" textstate2Text
---test_8_A_B_textstate_text_8 =   testVar2File result8A "resultB8" textstate2Text
+test_0_A_B_textstate_text_1 =   testVar2File result0A "resultB0" textstate2Text
+test_1_A_B_textstate_text_1 =   testVar2File result1A "resultB1" textstate2Text
+test_2_A_B_textstate_text_2 =   testVar2File result2A "resultB2" textstate2Text
+test_3_A_B_textstate_text_3 =   testVar2File result3A "resultB3" textstate2Text
+test_4_A_B_textstate_text_4 =   testVar2File result4A "resultB4" textstate2Text
+test_5_A_B_textstate_text_5 =   testVar2File result5A "resultB5" textstate2Text
+test_6_A_B_textstate_text_6 =   testVar2File result6A "resultB6" textstate2Text
+test_8_A_B_textstate_text_8 =   testVar2File result8A "resultB8" textstate2Text
 test_9_A_B_textstate_text_9 =   testVar2File result9A "resultB9" textstate2Text
 test_10_A_B_textstate_text_10 =   testVar2File result10A "resultB10" textstate2Text
 
