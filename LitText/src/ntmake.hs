5683a380992ef8f48b2aeaec8c06fe48af836bde
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2017-08-20 14:51:14 +0200

    added a local switch for using a local process for nlp

--- LitText/src/ntmake.hs
+++ LitText/src/ntmake.hs
@@ -33,9 +33,9 @@ import           Uniform.Convenience.StartApp hiding ((<>) , (</>), (<.>))
 import           Data.Semigroup               ((<>))
 import           Options.Applicative.Builder
 import           Options.Applicative
+import Producer.Servers
 
-
-programName = "may13 = ProduceLit" :: Text
+programName = "ntmake" :: Text
 progTitle = "produce the lit for all markup files " :: Text
 
 main :: IO ()
@@ -48,6 +48,7 @@ main = startProg programName progTitle
 --- cmd line parsing
 data LitArgs = LitArgs
   {  argOrigTest   :: Bool   -- ^ orig or test to decide where to take the file
+  , argLocalhost :: Bool -- ^ use localhost as fuseki server
   , argdir   :: String   -- ^ the subdirectory in originals
                         -- where the markup file is
                         -- the same dirname is used in convertsDir
@@ -61,6 +62,11 @@ cmdArgs = LitArgs
             short 'o' <>
 --            metavar "orig/test" <>
             help "orig or test" )
+     <*> switch
+          ( long "localhost" <>
+            short 'l' <>
+--            metavar "orig/test" <>
+            help "localhost or serverBrest" )
      <*> strOption
           ( long "author(dir)" <>
             short 'd' <>
@@ -86,11 +92,14 @@ parseAndExecute t1 t2    = do
         let dirs = if argOrigTest args
                 then dirsOrig
                 else dirsTest
+        let server = if  argLocalhost args
+                    then serverLocalhost
+                    else serverBrest
         if  null (argdir args)
             then
-                processAll False  dirs  serverBrest resfile
+                processAll False  dirs  server  resfile
             else do
-                let textstate = fillTextState3 dirs serverBrest
+                let textstate = fillTextState3 dirs server
                          (argdir args) (argbuch args)
                 mainLitAndNLPproduction False False textstate
                 -- first bool is debug output
