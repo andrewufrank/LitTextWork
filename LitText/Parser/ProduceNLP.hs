cba6cd2f4cb120acfb8759be5bb740996c18e656
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2017-09-02 17:40:01 +0200

    changed to pass textstate in a monadic fold, to avoid opening when already open

--- LitText/Parser/ProduceNLP.hs
+++ LitText/Parser/ProduceNLP.hs
@@ -50,20 +50,20 @@ produceNLP :: Bool -> TextDescriptor ->  [TZ2] -> ErrIO () -- test C  -> X
 -- produce the triples and store them in triple store,
 -- first extract only the text TZ lines and convert the hyphenated texts
 -- repeated for each paragraph
-produceNLP showXML textstate tzs = mapM_ (produceOneParaNLP showXML textstate) tzs
+produceNLP showXML textstate tzs = foldM_ (produceOneParaNLP showXML ) textstate tzs
 
 produceNLPnotshow = produceNLP False
 
 test_1_BAE_XproduceNLPtriples :: IO ()
 test_1_BAE_XproduceNLPtriples = testVar3FileIO result1A "resultBAE1" "resultX1" produceNLPnotshow
-test_2_BAE_XproduceNLPtriples = testVar3FileIO result2A "resultBAE2" "resultX2" produceNLPnotshow
-test_3_BAE_XproduceNLPtriples = testVar3FileIO result3A "resultBAE3" "resultX3" produceNLPnotshow
-test_4_BAE_XproduceNLPtriples = testVar3FileIO result4A "resultBAE4" "resultX4" produceNLPnotshow
-test_5_BAE_XproduceNLPtriples = testVar3FileIO result5A "resultBAE5" "resultX5" produceNLPnotshow
-test_6_BAE_XproduceNLPtriples = testVar3FileIO result6A "resultBAE6" "resultX6" produceNLPnotshow
-test_8_BAE_XproduceNLPtriples = testVar3FileIO result8A "resultBAE8" "resultX8" produceNLPnotshow
-test_9_BAE_XproduceNLPtriples = testVar3FileIO result10A "resultBAE9" "resultX9" produceNLPnotshow
-test_10_BAE_XproduceNLPtriples = testVar3FileIO result10A "resultBAE10" "resultX10" produceNLPnotshow
+--test_2_BAE_XproduceNLPtriples = testVar3FileIO result2A "resultBAE2" "resultX2" produceNLPnotshow
+--test_3_BAE_XproduceNLPtriples = testVar3FileIO result3A "resultBAE3" "resultX3" produceNLPnotshow
+--test_4_BAE_XproduceNLPtriples = testVar3FileIO result4A "resultBAE4" "resultX4" produceNLPnotshow
+--test_5_BAE_XproduceNLPtriples = testVar3FileIO result5A "resultBAE5" "resultX5" produceNLPnotshow
+--test_6_BAE_XproduceNLPtriples = testVar3FileIO result6A "resultBAE6" "resultX6" produceNLPnotshow
+--test_8_BAE_XproduceNLPtriples = testVar3FileIO result8A "resultBAE8" "resultX8" produceNLPnotshow
+--test_9_BAE_XproduceNLPtriples = testVar3FileIO result10A "resultBAE9" "resultX9" produceNLPnotshow
+--test_10_BAE_XproduceNLPtriples = testVar3FileIO result10A "resultBAE10" "resultX10" produceNLPnotshow
 ------ no result file is necessary, because result is zero
 ------ but results are found in LitTest/test
 --
@@ -77,16 +77,16 @@ testOP_E_F lang textstate inp =
 ----
 ----
 --test_1_E_F :: IO ()
-test_1_E_F = testVar3FileIO result1A "resultE1" "resultF1" (testOP_E_F German)
-test_2_E_F = testVar3FileIO result2A "resultE2" "resultF2" (testOP_E_F German)
-test_3_E_F = testVar3FileIO result3A "resultE3" "resultF3" (testOP_E_F German)
-test_4_E_F = testVar3FileIO result4A "resultE4" "resultF4" (testOP_E_F German)
-test_5_E_F = testVar3FileIO result5A "resultE5" "resultF5" (testOP_E_F English)
-test_6_E_F = testVar3FileIO result6A "resultE6" "resultF6" (testOP_E_F English)
-----test_7_E_F = testVar3FileIO result7A "resultE7" "resultF7" (testOP_E_F English)
-test_8_E_F = testVar3FileIO result8A "resultE8" "resultF8" (testOP_E_F English)
-test_9_E_F = testVar3FileIO result9A "resultE9" "resultF9" (testOP_E_F German)
-test_10_E_F = testVar3FileIO result10A "resultE10" "resultF10" (testOP_E_F English)
+--test_1_E_F = testVar3FileIO result1A "resultE1" "resultF1" (testOP_E_F German)
+--test_2_E_F = testVar3FileIO result2A "resultE2" "resultF2" (testOP_E_F German)
+--test_3_E_F = testVar3FileIO result3A "resultE3" "resultF3" (testOP_E_F German)
+--test_4_E_F = testVar3FileIO result4A "resultE4" "resultF4" (testOP_E_F German)
+--test_5_E_F = testVar3FileIO result5A "resultE5" "resultF5" (testOP_E_F English)
+--test_6_E_F = testVar3FileIO result6A "resultE6" "resultF6" (testOP_E_F English)
+------test_7_E_F = testVar3FileIO result7A "resultE7" "resultF7" (testOP_E_F English)
+--test_8_E_F = testVar3FileIO result8A "resultE8" "resultF8" (testOP_E_F English)
+--test_9_E_F = testVar3FileIO result9A "resultE9" "resultF9" (testOP_E_F German)
+--test_10_E_F = testVar3FileIO result10A "resultE10" "resultF10" (testOP_E_F English)
 ------ 9 german
 ------ 10 english
 
@@ -106,13 +106,13 @@ completeSentencesInDoc debugFlag textstate lang ( doc0) = do
         else return (  doc0)
 
 
-produceOneParaNLP :: Bool -> TextDescriptor -> TZ2 -> ErrIO ()
+produceOneParaNLP :: Bool -> TextDescriptor -> TZ2 -> ErrIO TextDescriptor
 produceOneParaNLP showXML textstate tzp = do
     (ntz,docs) :: (NLPtext,[Doc0]) <- convertTZ2nlp False showXML (nlpServer textstate) tzp  -- C -> E
-    mapM_  (produceOneOneParaNLP textstate ntz) (zip [1..] docs )
+    foldM  (produceOneOneParaNLP  ntz) textstate (zip [1..] docs )
 
-produceOneOneParaNLP :: TextDescriptor -> NLPtext -> (Int, Doc0)  -> ErrIO ()
-produceOneOneParaNLP textstate  ntz (snipnr, doc0)  =   do  -- tz is NLPtext
+produceOneOneParaNLP :: NLPtext -> TextDescriptor ->  (Int, Doc0)  -> ErrIO TextDescriptor
+produceOneOneParaNLP ntz textstate   (snipnr, doc0)  =   do  -- tz is NLPtext
         let lang = tz3lang ntz
         let paranr = tz3para $ ntz
         ( doc0') <- completeSentencesInDoc debugNLP1 textstate lang ( doc0)
@@ -126,64 +126,60 @@ produceOneOneParaNLP textstate  ntz (snipnr, doc0)  =   do  -- tz is NLPtext
         when debugNLP1 $
             putIOwords ["\n\nproduceOneParaNLP nlp triples "
                 , unlines' . map showT $ triples]
-                -- todo fileio add filepath to dir
---        let newFileName =  (authorDir textstate)
---                           ++ "/" ++ buchname textstate  ++ "." ++ ("nt"::FilePath) ::FilePath
---        filenameRes :: Path Abs File <- resolveFile (originalsDir  textstate)
---                           (newFileName::FilePath)
---        let textstate2 = textstate{textfilename=filenameRes}
         writeHandleTriples textstate triples
---        appendTriples2file textstate  triples
---            when debugNLP $ putIOwords ["produceOneParaNLP triples stored   "
---                        , showT . textfilename $ textstate, " \n", showT response ]
---            let response2 = response <>
---                        (showT . tlpara . tzloc $ tzp) <> "on page" <>
---                        (showT . tlpage . tzloc $ tzp)
-        return ()
+--        return ()
 
-
---appendTriples2file :: TextDescriptor -> [Triple] -> ErrIO ()
---appendTriples2file textstate tris = do
---    append6 (destNT textstate)  ntFileTriples tris
---    -- file must be deleted first!
---
---
---writeTriples2file :: TextDescriptor -> [Triple] -> ErrIO ()
---writeTriples2file textstate tris = do
---    write6 (destNT textstate)  ntFileTriples tris
---    -- this deletes previous file
 
 openHandleTriples  :: TextDescriptor -> ErrIO TextDescriptor
 openHandleTriples textstate  = do
     let mhand = destHandle textstate
     case mhand of
         Nothing ->  do
+                putIOwords ["openHandleTriples", "to", showT $ destNT textstate]
+--                let dir = getParentDir (destNT textstate)
+--                createDirIfMissing' dir
                 hand <- openHandle6 (destNT textstate)  ntFileTriples
                 return textstate{destHandle = Just hand}
             `catchError` \e -> do
-                putIOwords ["writeHandleTriples - error ", e ]
-                let dir = getParentDir (destNT textstate)
-                createDirIfMissing' dir
---                writeFileOrCreate2 (destNT textstate) (""::Text)
---                deleteFile (destNT textstate)
-                -- just to create the dir
-                openHandleTriples textstate
+                putIOwords ["openHandleTriples - error ", e ]
+--                openHandleTriples2 textstate
+                return textstate
 
-        Just hand ->  return textstate
+        Just hand -> do
+             putIOwords ["openHandleTriples is open", "to", showT $ destNT textstate]
+             return textstate
+
+--openHandleTriples2  :: TextDescriptor -> ErrIO TextDescriptor
+---- final stops if openhandle called recursively in error
+--openHandleTriples2 textstate  = do
+--    let mhand = destHandle textstate
+--    case mhand of
+--        Nothing ->  do
+--                hand <- openHandle6 (destNT textstate)  ntFileTriples
+--                return textstate{destHandle = Just hand}
+--            `catchError` \e -> do
+--                putIOwords ["writeHandleTriples - error ", e ]
+--                let dir = getParentDir (destNT textstate)
+--                createDirIfMissing' dir
+--                return textstate
+--
+--        Just hand ->  return textstate
 
 writeHandleTriples :: TextDescriptor -> [Triple] -> ErrIO TextDescriptor
 writeHandleTriples textstate tris = do
-                textstate2 <- openHandleTriples textstate
-                let hand = fromJustNote "writeHandleTriples" (destHandle textstate2)
-                writeHandle6 hand ntFileTriples tris
-                return textstate2
+    putIOwords ["writeHandleTriples", "to", showT $ destNT textstate]
+    textstate2 <- openHandleTriples textstate
+    let hand = fromJustNote "writeHandleTriples" (destHandle textstate2)
+    writeHandle6 hand ntFileTriples tris
+    return textstate2
 
 closeHandleTriples :: TextDescriptor ->  ErrIO TextDescriptor
 closeHandleTriples textstate = do
-                let hand = fromJustNote "writeHandleTriples" (destHandle textstate)
-                closeHandle6 (destNT textstate) ntFileTriples hand
-                let textstate2 = textstate{destHandle=Nothing}
-                return textstate2
+    putIOwords ["closeHandleTriples", "to", showT $ destNT textstate]
+    let hand = fromJustNote "closeHandleTriples" (destHandle textstate)
+    closeHandle6 (destNT textstate) ntFileTriples hand
+    let textstate2 = textstate{destHandle=Nothing}
+    return textstate2
 
 
 
