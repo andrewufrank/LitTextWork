288f67c1b8e31a4ddc11ddcb11fad4fdb1517d62
frank <frank@prag.local>
2017-11-13 21:50:46 -0500

    cleaned up and simplified

--- LitText/Parser/ProduceLit.hs
+++ LitText/Parser/ProduceLit.hs
@@ -111,7 +111,7 @@ convOneTextZeile2triple :: TextDescriptor -> TZ2 -> [Triple]
 -- produce all triples necessary for each line item
 -- not executed when text not included
 convOneTextZeile2triple textstate tz  = case tz of
-    TZ2markup {} -> hlTriple textstate (tz2tok tz) tz
+    TZ2markup {} -> hlTriple textstate  tz
                       ++ (if tz2tok tz `elem` [BuchKlappenText .. BuchIV3]
                             then  otherBuchTriple textstate tz
                             else []
@@ -154,9 +154,9 @@ inWerkTriple textstate sigl =   mkTripleRef sigl  (mkRDFproperty InWerk)
                                     (buchURIx textstate)
 -- probably not needed ??
 
-hlTriple :: TextDescriptor -> BuchToken -> TZ2 -> [Triple]
+hlTriple :: TextDescriptor ->   TZ2 -> [Triple]
 -- ^ produce the triples for header levels
-hlTriple textstate mk tz =   if includeText textstate
+hlTriple textstate  tz =   if includeText textstate
         then
             [ mkTripleLang lang (unParaSigl sigl) (mkRDFproperty Text)
                 (twm $ tz2text tz)
@@ -172,6 +172,7 @@ hlTriple textstate mk tz =   if includeText textstate
         sigl = paraSigl textstate . tz2para $  tz
         inSigl = paraSigl textstate . tz2inPart $ tz
         lang = tz2lang tz
+        mk = tz2tok tz
 
 
 --paraTriple :: TextDescriptor -> TZ2 -> [Triple]
@@ -203,13 +204,13 @@ hlTriple textstate mk tz =   if includeText textstate
 test_1BAE_H = testVar3File result1A "resultBAE1" "resultH1" produceLitTriples
 test_2BAE_H = testVar3File result2A "resultBAE2" "resultH2" produceLitTriples
 test_3BAE_H = testVar3File result3A "resultBAE3" "resultH3" produceLitTriples
---test_4BAE_H = testVar3File result4A "resultBAE4" "resultH4" produceLitTriples
---test_5BAE_H = testVar3File result5A "resultBAE5" "resultH5" produceLitTriples
---test_6BAE_H = testVar3File result6A "resultBAE6" "resultH6" produceLitTriples
-----test_7BAE_H = testVar3File result7A "resultBAE7" "resultH7" produceLitTriples
---test_8BAE_H = testVar3File result8A "resultBAE8" "resultH8" produceLitTriples
---test_9BAE_H = testVar3File result9A "resultBAE9" "resultH9" produceLitTriples
---test_10BAE_H = testVar3File result10A "resultBAE10" "resultH10" produceLitTriples
+test_4BAE_H = testVar3File result4A "resultBAE4" "resultH4" produceLitTriples
+test_5BAE_H = testVar3File result5A "resultBAE5" "resultH5" produceLitTriples
+test_6BAE_H = testVar3File result6A "resultBAE6" "resultH6" produceLitTriples
+--test_7BAE_H = testVar3File result7A "resultBAE7" "resultH7" produceLitTriples
+test_8BAE_H = testVar3File result8A "resultBAE8" "resultH8" produceLitTriples
+test_9BAE_H = testVar3File result9A "resultBAE9" "resultH9" produceLitTriples
+test_10BAE_H = testVar3File result10A "resultBAE10" "resultH10" produceLitTriples
 
 
 writeLitTriples :: FilePath -> FilePath ->  IO ()
@@ -229,12 +230,12 @@ writeLitTriples source dest   = do
 test_1H_K = writeLitTriples   "resultH1" "resultK1"
 test_2H_K = writeLitTriples   "resultH2" "resultK2"
 test_3H_K = writeLitTriples   "resultH3" "resultK3"
---test_4H_K = writeLitTriples   "resultH4" "resultK4"
---test_5H_K = writeLitTriples   "resultH5" "resultK5"
---test_6H_K = writeLitTriples   "resultH6" "resultK6"
---test_7H_K = writeLitTriples   "resultH7" "resultK7"
---test_8H_K = writeLitTriples   "resultH8" "resultK8"
---test_9H_K = writeLitTriples   "resultH9" "resultK9"
---test_10H_K = writeLitTriples   "resultH10" "resultK10"
+test_4H_K = writeLitTriples   "resultH4" "resultK4"
+test_5H_K = writeLitTriples   "resultH5" "resultK5"
+test_6H_K = writeLitTriples   "resultH6" "resultK6"
+test_7H_K = writeLitTriples   "resultH7" "resultK7"
+test_8H_K = writeLitTriples   "resultH8" "resultK8"
+test_9H_K = writeLitTriples   "resultH9" "resultK9"
+test_10H_K = writeLitTriples   "resultH10" "resultK10"
 
 
