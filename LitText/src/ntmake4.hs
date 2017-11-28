831fbbfe159e6920c3e732dc88113e3803634fc1
frank <frank@prag.local>
2017-11-28 10:22:14 -0500

    removed old code

--- LitText/src/ntmake4.hs
+++ LitText/src/ntmake4.hs
@@ -22,18 +22,7 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 
---import qualified Pipes as Pipe
---import qualified Pipes.Prelude as Pipe
---import Pipes ((>->), (~>))
-
---import Processor.ProcessAll hiding ((<>) , (</>), (<.>))
---import           Parser.Foundation ()
---     hiding ((<>) , (</>), (<.>))
--- import           Parser.LinesToParagrahs
 import           Uniform.FileIO         hiding ((<>))
---import           Uniform.Strings              hiding ((<>), (</>), (<.>))
---import Lines2para.Lines2ignore (LanguageCode, readLanguageCode)
---            hiding ((<>) , (</>), (<.>))
 
 import           Uniform.Convenience.StartApp hiding ((<>) , (</>), (<.>))
 
@@ -68,13 +57,6 @@ parseAndExecute t1 t2    = do
         let args = setDefaultOriginDirNT args1 (addDir homeDir ("gutenberg"::FilePath))
         putIOwords ["parseAndExecute: process all store", showT args]
         let server = selectServerNT args :: URI
---    args <- callIO $ execParser opts
---    let homedir = makeAbsDir "/home/frank/"
---    putIOwords ["ntstore4 parseAndExecute dir", showT args]
---    let server = if  argLocalhost args
---                    then serverLocalhost
---                    else serverBrest
---        let ntdir =   (makeAbsDir $ argOrigin args) :: Path Abs Dir
 
 --    putIOwords ["ntstore4 parseAndExecute all files", showT server, " from nt dir", showT ntdir ]
 
@@ -83,13 +65,7 @@ parseAndExecute t1 t2    = do
         -- not really interesting
         let forceFlag = argForceFlag args
         let debugFlag = False
---        let dbarg = s2t . argDB $ args
---        let mgraph = Just . s2t $ argGraph args
---        let fn = getFn args -- addFileName homedir  (makeRelFile $ argFn args) :: Path Abs File
         let
---             timeout = getTimeout args
---             mgraph = Just . s2t $ argGraph args
---             dbarg = s2t $ argDB args
              originDir = makeAbsDir  $  addDir homeDir (argOrigin args) :: Path Abs Dir
              destinationDir = makeAbsDir $  addDir homeDir ( argDestination args) :: Path Abs Dir
              authorReplacement = s2t . getNakedDir $ originDir
@@ -141,11 +117,6 @@ data NtmakeArgs = NtmakeArgs
   {
      argLocalhost :: Bool -- ^ use localhost as fuseki server
      , argDestination :: String -- ^ the directory for the nt files
---   , argSource :: String -- ^ the directoy in which the markup files are
---  , argdir   :: String   -- ^ the subdirectory in originals
---                        -- where the markup file is
---                        -- the same dirname is used in convertsDir
---  , argbuch  :: String -- ^ the filename in the dir
    , argOrigin :: String -- ^ the directoy in which the markup files are
    , argForceFlag :: Bool -- ^ force processing, even if newer exist
   } deriving (Show)
@@ -157,32 +128,12 @@ ntcmdArgs = NtmakeArgs
             short 'l' <>
 --            metavar "orig/test" <>
             help "localhost not serverBrest (default)" )
---     <*> switch
---          ( long "query" <>
---            short 'q' <>
-----            metavar "orig/test" <>
---            help "query (not update)" )
      <*> strOption
           ( long "destinationDir" <>
             short 'd' <>
             value "" <>
             metavar "Destination directory"
             <> help "directory for the nt files " )
---     <*> strOption
---        (long "sourcedir" <>
---            short 's'  <>
---            help "dir in which the markup files are (relative to origin dir)" )
---      <*> switch
---          ( long "localhost" <>
---            short 'l' <>
-----            metavar "orig/test" <>
---            help "localhost or serverBrest" )
---     <*> strOption
---          ( long "buch(file)" <>
---            short 'f' <>
---            value "" <>
---            metavar "buch"
---            <> help "buch - filename " )
      <*>  strOption
         (long "origin dir" <>
             short 'o'  <>
@@ -197,53 +148,6 @@ ntcmdArgs = NtmakeArgs
 
 
 
---parseAndExecute  :: Text -> Text ->  ErrIO ()
---parseAndExecute t1 t2    = do
---        args <- callIO $ execParser opts
-----        let lang = readLanguageCode "readLangCode in MainCollect"
-----                    (s2t $ argLanguage args) :: LanguageCode
---
---        let homedir = makeAbsDir "/home/frank" :: Path Abs Dir
---        let markupdir = addDir homedir (argSource args :: FilePath) :: Path Abs Dir
---        let resfile  = addFileName homedir $ makeRelFile "resultCollect" :: Path Abs File
---        let ntdir = addDir homedir (argTarget args) :: Path Abs Dir
---        let authorReplacement = s2t . getNakedDir . argSource $ args
---        processAll4 False markupdir serverBrest ntdir resfile authorReplacement
-----
-----        let server = if  argLocalhost args
-----                    then serverLocalhost
-----                    else serverBrest
-----        if  null (argdir args)
-----            then
-----                processAll False  dirs  server  resfile
-----            else do
-----                let textstate = fillTextState3 dirs server
-----                         (argdir args) (argbuch args)
-----                mainLitAndNLPproduction False False textstate
---                -- first bool is debug output
---                -- second stops processing after lit (no nlp calls)
-----                return ()
-----        processAll False dir (s2t . argDB $ args) (Just . s2t $ argGraph args) resultFile
-----        -- true for debug stores only the first 3 triples...
---        return ()
-
---processAll4 :: Bool ->  Path Abs Dir-> URI -> Path Abs Dir  -> Path Abs File  -> Text -> ErrIO ()
----- | get all markup files in the partially filled TextState2
----- the output goes to the second, not the triples
----- debug flag is not yet used
---
---processAll4 debug path  server ntdir resfile authorReplacement =
-----  let path = source litdirs
---  bracketErrIO (openFile2handle resfile WriteMode)
---        (closeFile2)
---        (\hand ->
---              Pipe.runEffect $
---                getRecursiveContents path
---                >-> Pipe.filter isMarkup --
---                >-> Pipe.mapM (fmap t2s . processOneMarkup4 debug server ntdir authorReplacement)
---            --    >-> P.stdoutLn
---                >-> Pipe.toHandle hand
---        )
 
 isMarkup :: Path Abs File -> Bool
 isMarkup  = hasExtension (Extension "markup")
