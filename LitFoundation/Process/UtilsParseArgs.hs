412bf4c6fd4f8f507fa1d91804633314c6458c69
frank <frank@prag.local>
2017-12-07 14:01:54 -0500

    added the parse utilities here

---
+++ LitFoundation/Process/UtilsParseArgs.hs
@@ -0,0 +1,183 @@
+ -----------------------------------------------------------------------------
+--
+-- Module      :  utils
+-- Copyright   :  andrew u frank -
+--
+-- | utilities for the query and storage processing
+-----------------------------------------------------------------------------
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
+
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fno-warn-missing-methods #-}
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+{-# OPTIONS_GHC -w #-}
+
+
+module Process.UtilsParseArgs
+    (module Process.UtilsParseArgs
+        , dirQueries, URI
+
+--    , URI, serverBrest, serverLocalhost
+    )
+    where
+
+import           Test.Framework
+import           Uniform.FileIO hiding ((<>), (</>), (<.>))
+--import           Uniform.Strings
+--import           Uniform.Error
+
+import           Data.Semigroup               ((<>))
+import           Options.Applicative.Builder
+import           Options.Applicative
+
+import Producer.Servers (serverLocalhost, serverBrest, rdfBase, dirQueries, URI)
+import Uniform.HttpCallWithConduit (callHTTP8post, addPort2URI, callHTTP10post, URI)
+
+-- should be imported from Foundation
+
+--- cmd line parsing
+data LitArgs = LitArgs
+  {
+--    argOrigTest   :: Bool   -- ^ orig or test to decide where to take the file
+    argLocalhost :: Bool -- ^ use localhost as fuseki server
+--  , argQuery :: Bool -- ^ query to produce file, not an update
+  , argDB :: String -- ^ the database name
+  , argGraph  :: String  -- ^ the graph
+  , argAuxGraph  :: String  -- ^ an additional graph
+--  , argDest  :: String  -- ^ the destination graph
+  , argDir  :: String  -- ^ the query dir
+  , argFn  :: String -- ^ the query filename without extension
+--  , argResultDir :: String -- ^ the result dir
+  , argWordnetGraph  :: String -- ^ the name of the wordnet graph without <..>
+  , argTimeout  :: String -- ^ the timeout (if any) in minutes
+   , argOrigin :: String -- ^ the directoy in which the markup files are
+   , argForceFlag :: Bool -- ^ force processing, even if newer exist
+    , argBookNrFile :: String -- ^ a single book (not a full link)
+
+  } deriving (Show)
+
+cmdArgs :: Parser LitArgs
+cmdArgs = LitArgs
+     <$> switch
+          ( long "localhost" <>
+            short 'l' <>
+--            metavar "orig/test" <>
+            help "localhost not serverBrest (default)" )
+--     <*> switch
+--          ( long "query" <>
+--            short 'q' <>
+----            metavar "orig/test" <>
+--            help "query (not update)" )
+     <*> strOption
+          ( long "database (was corpus)" <>
+            short 'd' <>
+        --   long "subdir" <>
+            metavar "db (was corpus) - required " <>
+            help "dbname from fuseki (was corpus)" )
+     <*> strOption
+          ( long "graph - required" <>
+            short 'g' <>
+            metavar "Graph" <>
+            help "graph - required" )
+     <*> strOption
+          ( long "auxGraph" <>
+            short 'a' <>
+            value "" <>
+            metavar "Auxilliary Graph" <>
+            help "graph" )
+--     <*> strOption
+--          ( long "dest" <>
+--            short 'd' <>
+--            value "" <>
+--            metavar "Destination Graph" <>
+--            help "destination graph" )
+     <*> strOption
+          ( long "subdir" <>
+            short 's' <>
+            value "" <>
+            metavar "subdir (folder) with the construct queries" <>
+            help "subdir (folder) with the construct queries to execute" )
+     <*> strOption
+          ( long "filename of construct query" <>
+            short 'f' <>
+            value "" <>
+            metavar "query (filename)" <>
+            help "filename (without .construct), will also be name of resulting .ttl" )
+--     <*> strOption
+--          ( long "result dir" <>
+--            short 'r' <>
+--            value "/home/frank/constructResults" <>
+--            metavar "result dir" <>
+--            help "filename (without .construct), will also be name of resulting .ttl" )
+     <*> strOption
+          ( long "wordnet" <>
+            short 'w' <>
+            value "" <> -- "http://nlp.gerastree.at:3030/corpus3/data/wn31" <>
+            metavar "wordnet graph" <>
+            help "the wordnet graph without <..> " )
+     <*> strOption
+          ( long "timeout" <>
+            short 't' <>
+            value "" <>
+            metavar "timeout" <>
+            help "timeout in minutes " )
+     <*>  strOption
+        (long "origin dir" <>
+            short 'o'  <>
+            value "" <>
+            help "dir in which the nt?, markup or query files are (relative to home)" )
+     <*> switch
+          ( long "force" <>
+--            value False <>
+--            short 'l' <>
+--            metavar "force processing " <>
+            help "force processing even when newer exist (default false)" )
+     <*>  strOption
+        (long "input file with the booknumbers " <>   -- must be synced manually to brest?
+            short 'i'  <>
+            value "" <> -- set to in main /home/frank/gutenberg" <>
+            help "input file with the booknumbers - cvs, booknr in first column" )
+
+setDefaultOriginDir :: LitArgs -> String -> LitArgs
+setDefaultOriginDir args def = if null' $ argOrigin args then  args {argOrigin = def}
+                                                else args
+
+getArgsParsed :: Text -> Text -> ErrIO LitArgs
+getArgsParsed t1 t2 = do
+        args <- callIO $ execParser opts
+        return args
+    where
+        opts = info (helper <*> cmdArgs)
+          ( fullDesc
+         <> (progDesc . t2s $ t1)
+         <> (header . t2s $ t2 ))
+
+selectServer :: LitArgs -> URI
+-- select the server between localhost and brest
+selectServer args =  if  argLocalhost args
+                    then serverLocalhost
+                    else serverBrest  -- default
+
+getLocalOriginDir args = if null' subdir then orig
+                                        else addDir  orig subdir :: Path Abs Dir
+                where subdir = argDir args
+--                      dirQueries1 = if null nonDef
+--                                            then (dirQueries :: Path Abs Dir)
+--                                            else makeAbsDir nonDef
+                      orig = makeAbsDir $ argOrigin args
+
+getFn args = addFileName (getLocalOriginDir args) (argFn args) :: Path Abs File
+
+--test_queryDir = assertEqual "" (addDir
+
+getTimeout :: LitArgs -> Maybe Int
+getTimeout args = fmap (60 *) t1
+    where
+--        t1 = readMay ("30"::String) :: Maybe Int
+        t1 = readMay (argTimeout args)  :: Maybe Int
+
+
+--        D
---
+++ LitFoundation/Process/UtilsProcessing.hs
@@ -0,0 +1,93 @@
+ -----------------------------------------------------------------------------
+--
+-- Module      :  utils
+-- Copyright   :  andrew u frank -
+--
+-- | utilities for the query and storage processing
+-----------------------------------------------------------------------------
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
+
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fno-warn-missing-methods #-}
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+{-# OPTIONS_GHC -w #-}
+
+
+module Process.UtilsProcessing
+    (module Process.UtilsProcessing
+--        , module Process.UtilsParseArgs
+        , getTimeout
+        , dirQueries, URI
+
+--    , URI, serverBrest, serverLocalhost
+    )
+    where
+
+import           Test.Framework
+import           Uniform.FileIO hiding ((<>), (</>), (<.>))
+import Process.UtilsParseArgs
+import Producer.Servers (serverLocalhost, serverBrest, rdfBase, dirQueries, URI)
+import Uniform.HttpCallWithConduit (callHTTP8post, addPort2URI, callHTTP10post, URI, HttpQueryString)
+import           Uniform.Strings
+
+
+import qualified Pipes as Pipe
+import qualified Pipes.Prelude as Pipe
+import Pipes ((>->), (~>))
+-- todo fileio - export for pipes
+
+processAll :: (Path Abs File -> ErrIO Text) -> (Path Abs File -> Bool)
+        -> Path Abs Dir -> Path ar File  -> ErrIO Text
+-- | get all   files passing the test from the dir and process them with the ops
+-- the file is the result report, not informativ
+processAll ops testFile dir file = do  -- debug forceFlag server dir db mgraph  file =  do
+    putIOwords ["processAllNTGZ", "dir", showT dir, "file", showT file]
+--            db - graph", db, showT mgraph, "debug", showT debug]
+    let path = toFilePath dir
+    resFile <- makeAbsolute file
+    bracketErrIO (openFile2handle resFile WriteMode)
+                closeFile2  -- not with transaction tmp
+                (\hand ->
+                      Pipe.runEffect $
+                        getRecursiveContents dir
+                        >-> Pipe.filter testFile --
+                        >-> Pipe.mapM (fmap t2s . ops) --  putOneFile2xx debug forceFlag server db mgraph)
+                    --    >-> P.stdoutLn
+                        >-> Pipe.toHandle hand
+                )
+    return $ unwords' ["processAll", showT dir, showT file , "ok"]
+
+--putOneFile2xx :: Bool -> Bool -> URI -> Text -> Maybe Text -> Path Abs File -> ErrIO Text
+---- put one file into the db and graph
+---- checks for the flag to avoid duplication
+---- but does separately check (and possibly duplicate) an nt and a nt.gz file
+--
+--putOneFile2xx debug forceFlag server db mgraph fn = return ""
+
+getServer server  = showT $ addPort2URI  server  3030
+
+
+post2store ::  Bool -> Text -> Text -> Text -> Maybe Text ->  LazyByteString
+            -> HttpQueryString -> Maybe Int ->   ErrIO Text
+    -- ^ timeout in sec
+
+-- use post with multipart form to store (see https://www.w3.org/TR/sparql11-http-rdf-update/#http-post)
+post2store debug appType fusekiServer pathName mgraph split qstring timeout  = do
+    -- form the instert query
+    let pathNamePlusGraph = pathName <> "?" <>
+             maybe "default"  (\t -> "graph=" <> t)
+                    mgraph
+--    when debug $
+--    putIOwords ["\npost2store for",  fn, "path", pathNamePlusGraph ]
+    putIOwords ["post2store", "pathNamePlusGraph",   pathNamePlusGraph, "qstring", showT qstring]
+
+    res <- callHTTP10post True appType
+                fusekiServer pathNamePlusGraph
+                split qstring   timeout
+    when True $ putIOwords ["post2store done", showT res]
+    return res
+
+--        D
