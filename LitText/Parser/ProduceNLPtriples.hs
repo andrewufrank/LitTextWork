bfa925b4ba4494d61b0271fccd425fe38c16f7ac
frank <frank@prag.local>
2018-01-08 12:02:23 -0500

    writing G and L uses NoLanguage because the snip with the language code is not available

--- LitText/Parser/ProduceNLPtriples.hs
+++ LitText/Parser/ProduceNLPtriples.hs
@@ -177,7 +177,7 @@ mkDependencePart2 lang sentid depidp gd depp   = [t8] -- , t9]
 
 testOP_E_G :: TextDescriptor -> [Doc0] ->  [Triple]
 testOP_E_G textstate docs  = concat
-        . map (processDoc0toTriples2 textstate English (ParaNum 99))
+        . map (processDoc0toTriples2 textstate NoLanguage (ParaNum 99))
         $ (zip [1..] docs)
 -- here missing the values for language and paranr
 -- fake paranr 99 should be ok for test
@@ -189,13 +189,22 @@ test_3_E_G =  testVar3File result3A "resultE3" "resultG3" testOP_E_G
 test_4_E_G =  testVar3File result4A "resultE4" "resultG4" testOP_E_G
 test_5_E_G =  testVar3File result5A "resultE5" "resultG5" testOP_E_G
 test_6_E_G = testVar3File result6A "resultE6" "resultG6" testOP_E_G
---test_7_E_G = testVar3File result6A "resultE7" "resultG7" testOP_E_G
+test_7_E_G = testVar3File result6A "resultE7" "resultG7" testOP_E_G
 test_8_E_G = testVar3File result8A "resultE8" "resultG8" testOP_E_G
---test_9_E_G = testVar3File result9A "resultE9" "resultG9" testOP_E_G
---test_10_E_G = testVar3File result10A "resultE10" "resultG10" testOP_E_G
+test_9_E_G = testVar3File result9A "resultE9" "resultG9" testOP_E_G
+test_10_E_G = testVar3File result10A "resultE10" "resultG10" testOP_E_G
 ---- 10 seems too big for oporto (without swap)
 --
-
+test_1G_L = writeLitTriples   "resultG1" "resultL1"
+test_2G_L = writeLitTriples   "resultG2" "resultL2"
+test_3G_L = writeLitTriples   "resultG3" "resultL3"
+test_4G_L = writeLitTriples   "resultG4" "resultL4"
+test_5G_L = writeLitTriples   "resultG5" "resultL5"
+test_6G_L = writeLitTriples   "resultG6" "resultL6"
+test_7G_L = writeLitTriples   "resultG7" "resultL7"
+test_8G_L = writeLitTriples   "resultG8" "resultL8"
+test_9G_L = writeLitTriples   "resultG9" "resultL9"
+test_10G_L = writeLitTriples   "resultG10" "resultL10"
 ------------ coreferences ---------------------
 
 mkCorefTriple2 :: LanguageCode -> SnipSigl ->     Coref0 -> CorefNr ->  [Triple]
