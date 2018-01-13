6bb4d9928889a3867cf577e635ce044602943948
frank <frank@prag.local>
2018-01-13 15:45:30 -0500

    new module for UD codes, requires chatter and cereal

---
+++ LitText/CoreNLP/POScodesUD.hs
@@ -0,0 +1,77 @@
+{-----------------------------------------------------------------------------
+--
+-- Module       | --  Dependency and other Codes
+--
+-- | the codes for UD  -- the table is lifted
+--
+-----------------------------------------------------------------------------}
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
+{-# LANGUAGE        MultiParamTypeClasses
+       , ScopedTypeVariables
+        , FlexibleContexts
+    , OverloadedStrings
+        , TypeSynonymInstances
+        , FlexibleInstances
+        , DeriveAnyClass
+        , DeriveGeneric
+        #-}
+
+module CoreNLP.POScodesUD (module CoreNLP.POScodesUD
+--        , module NLP.Corpora.Conll
+        )
+         where
+
+import GHC.Generics
+import Data.Serialize (Serialize)
+
+import           Test.Framework
+
+import Uniform.Zero
+import Uniform.Strings
+import Uniform.Error
+
+import qualified NLP.Types.Tags as NLPtypes
+--import      NLP.Corpora.Conll
+--import      NLP.Corpora.Conll   as Conll
+
+data PosTagUD =   -- copied from http://universaldependencies.org/u/pos/
+    ADJ | -- adjective
+    ADP | -- adposition
+    ADV | -- adverb
+    AUX | -- auxiliary
+    CCONJ | -- coordinating conjunction
+    DET | -- determiner
+    INTJ | -- interjection
+    NOUN | -- noun
+    NUM | -- numeral
+    PART | -- particle
+    PRON | -- pronoun
+    PROPN | -- proper noun
+    PUNCT | -- punctuation
+    SCONJ | -- subordinating conjunction
+    SYM | -- symbol
+    VERB | -- verb
+    X  -- other
+        deriving (Read, Show, Ord, Eq, Generic, Enum, Bounded)
+
+
+instance NLPtypes.Tag PosTagUD where
+--parseTag :: Text -> PosTag
+    parseTag txt = case readTag txt of
+                       Left  _ -> X
+                       Right t -> t
+
+readTag :: Text -> ErrOrVal PosTagUD
+readTag txt = maybe2errorP . read . t2s $ txt
+
+maybe2errorP  :: Maybe a -> ErrOrVal a
+maybe2errorP Nothing = Left "readTag PosTagUD 34232"
+maybe2errorP (Just a) = Right a
+
+instance Serialize PosTagUD
+
+instance Zeros PosTagUD where zero = X
+--type Unk = Conll.Unk
+
+
+
