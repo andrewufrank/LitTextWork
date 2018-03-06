5550af409f7606e4a5a2f6bd5981863096cf0571
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2018-03-06 23:37:16 +0100

    fixed tests

--- LitText/tests/Parser/ProduceNLP_test.hs
+++ LitText/tests/Parser/ProduceNLP_test.hs
@@ -43,13 +43,13 @@ import Uniform.FileIO (getAppUserDataDir')
 testOP_DA_L :: TextDescriptor -> [Snip]-> ErrIO [[Triple]]
 testOP_DA_L textstate = mapM (convertOneSnip2Triples  [] textstate )
 
---test_1_DA_L = testVar3FileIO result1A "resultDA1" "resultE1" testOP_DA_L
+test_1_DA_L = testVar3FileIO result1A "resultDA1" "resultE1" testOP_DA_L
 --test_2_DA_L = testVar3FileIO result2A "resultDA2" "resultE2" testOP_DA_L
 --test_3_DA_L = testVar3FileIO result3A "resultDA3" "resultE3" testOP_DA_L
 --test_4_DA_L = testVar3FileIO result4A "resultDA4" "resultE4" testOP_DA_L
 --test_5_DA_L = testVar3FileIO result5A "resultDA5" "resultE5" testOP_DA_L  -- lafayette
 --test_6_DA_L = testVar3FileIO result6A "resultDA6" "resultE6" testOP_DA_L
-test_8_DA_L = testVar3FileIO result8A "resultDA8" "resultE8" testOP_DA_L
+--test_8_DA_L = testVar3FileIO result8A "resultDA8" "resultE8" testOP_DA_L
 ----test_9_DA_L = testVar3FileIO result9A "resultDA9" "resultE9" testOP_DA_L  -- multiple language - not a single head
 --test_10_DA_L = testVar3FileIO result10A "resultDA10" "resultE10" testOP_DA_L
 --test_11_DA_L = testVar3FileIO result11A "resultDA11" "resultE11" testOP_DA_L
@@ -114,7 +114,7 @@ produceNLPtest2 textstate startfile resfile  = do
     return ()
 
 --test_1_BAE_XproduceNLPtriples :: IO ()
---test_1_BAE_XproduceNLPtriples = testVar3FileIO result1A "resultBAE1" "resultX1" produceNLPtest
+test_1_BAE_XproduceNLPtriples = testVar3FileIO result1A "resultBAE1" "resultX1" produceNLPtest
 --test_2_BAE_XproduceNLPtriples = testVar3FileIO result2A "resultBAE2" "resultX2" produceNLPtest
 --test_3_BAE_XproduceNLPtriples = testVar3FileIO result3A "resultBAE3" "resultX3" produceNLPtest
 --test_4_BAE_XproduceNLPtriples = testVar3FileIO result4A "resultBAE4" "resultX4" produceNLPtest
